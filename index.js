93ebd96386a88e881fd0bb8d9816134100608762
Kaper <48963664+Kaper365@users.noreply.github.com>
2020-08-16 19:51:38 +0200

    removing command left in dev phase

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ client.on("guildCreate", guild => {
 });
 
 client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
